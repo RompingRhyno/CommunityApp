fefeb52e9043c9e4dac887f36b13c2ab58f1b458
ryan94j@gmail.com <rhino@archrhino.localdomain>
2025-01-24 14:27:50 -0800

    add database migration to program.cs

--- Program.cs
+++ Program.cs
@@ -46,4 +46,11 @@ app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
 
+using (var scope = app.Services.CreateScope()) 
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();     
+    context.Database.Migrate();
+}
+
 app.Run();
